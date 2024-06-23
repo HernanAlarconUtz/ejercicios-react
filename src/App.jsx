238d9d3273955b508f1f9d78c60b6f49a2b8b4b3
Hernan Alarcon Utz <hernanalarcon51@gmail.com>
2024-06-23 19:05:16 -0300

    Se realizo el ejercicio 4 del trabajo practico de React

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
+import Ejercicio4 from "./components/Ejercicio4/Ejercicio4";
 // import Ejercicio2 from "./components/Ejercicio2/Ejercicio2";
-import Ejercicio3 from "./components/Ejercicio3/Ejercicio3";
+// import Ejercicio3 from "./components/Ejercicio3/Ejercicio3";
 // import Ejercicio1 from "./components/ejercicio1/Ejercicio1"
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     <>
       {/* <Ejercicio1 /> */}
       {/* <Ejercicio2 message="my friend" /> */}
-      <Ejercicio3 />
+      {/* <Ejercicio3 /> */}
+      <Ejercicio4 />
     </>
   );
 }
