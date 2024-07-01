b1b1f07ccff9c9125837d2e8e3780703b53f7397
Hernan Alarcon Utz <hernanalarcon51@gmail.com>
2024-06-30 22:17:12 -0300

    Se realizo el ejercicio 7 del tp de React

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import Ejercicio6 from "./components/Ejercicio6.jsx/Ejercicio6";
+// import Ejercicio6 from "./components/Ejercicio6.jsx/Ejercicio6";
+import Ejercicio7 from "./components/Ejercicio7/Ejercicio7";
 // import Ejercicio4 from "./components/Ejercicio4/Ejercicio4";
 // import Ejercicio5 from "./components/Ejercicio5/Ejercicio5";
 // import Ejercicio2 from "./components/Ejercicio2/Ejercicio2";
@@ -14,7 +15,8 @@ function App() {
       {/* <Ejercicio3 /> */}
       {/* <Ejercicio4 /> */}
       {/* <Ejercicio5 /> */}
-      <Ejercicio6 />
+      {/* <Ejercicio6 /> */}
+      <Ejercicio7 />
     </>
   );
 }
